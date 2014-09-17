754d03c93942a3e45f8d7093eeea71e3b1704c38
Jens Edhammer <tobyndax@hotmail.com>
2014-09-17 17:13:37 +0200

    Fixed factor 2 issue. Real signal ifft should use symmetric argument?

--- Task1.m
+++ Task1.m
@@ -1,16 +1,19 @@
 %TSDT14 JENS OCH DAVID task1
 
 %frekvens- och tidsvektor
-f = 0:0.01:99.99;
-t = 0:0.01:99.99;
+f = 0:0.01:99;
+t = 0:0.01:99;
 %PSD av vitt Gaussian brus
 Rx = 10;
 %Simpelt lågpassfilter H(f) = 1/(1+jf/fc)
 %fc är skärfrekvens
+fc = 10;
 
-H1 = 1./(1+j*f/fc);
+H1 = 1./(1+1i*f/fc);
 Ry1 = abs(H1).^2 * Rx;
-ry1 = 1/0.01*ifft(Ry1); %fel med en faktor 2
+dt = 0.01;
+ry1 = ifft(Ry1,'symmetric')/dt; %symmetric, due to real signal?
+
 
 Ryt1 = Rx./(1+(f/fc).^2);
 ryt1 = Rx*pi*fc*exp(-2*pi*fc*abs(t));
@@ -43,7 +46,7 @@ Wc = 2*pi*fc;
 [b,a] = butter(10,Wc,'s');
 
 Ry2 = abs(H2).^2 * Rx;
-ry2 = 400*ifft(Ry2);
+ry2 = ifftshift(1/(0.01)*ifft(fftshift(Ry2)));
 
 figure(2)
 subplot(221);
