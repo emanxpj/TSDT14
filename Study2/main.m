2eba9256a928c4cf54bc7bcd6b501cd6589c3799
habrman <davha227@student.liu.se>
2014-10-01 10:45:47 +0200

    Periodogram för study2

--- Study2/main.m
+++ Study2/main.m
@@ -1,5 +1,7 @@
 addpath ./Study1;
 
+%--------Skapar filtrerat brus------------------------------
+
 N = 2^10;
 N2 = N+1;
 Ts = 1; %length of the measured signal.
@@ -10,7 +12,100 @@ x = randn(1,N);
 
 
 n = linspace(0,N,N); 
-wc = 0.4;
+wc = 0.1;
 [b,a] = butter(10,wc,'low');
 
 y = filter(b,a,x);
+t = linspace(-N/2,N/2,N);
+% figure(1)
+% subplot(211)
+% plot(t,x);xlim([-N/2 N/2]);
+% subplot(212)
+ plot(t,y);xlim([-N/2 N/2]);
+%%
+%-----------Skapar system---------------------------
+
+theta0 = (wc-0.05); %vad är ett lämpligt värde?
+zsq = y.^2;
+
+zhw = zeros(1,N);
+zhw(y>0) = y(y>0);
+
+zam = y.*cos(theta0*n);
+%plot av sytemsignaler
+figure(2)
+subplot(221)
+plot(t,zsq);xlim([-N/2 N/2]);
+subplot(222)
+plot(t,zhw);xlim([-N/2 N/2]);
+subplot(223)
+plot(t,zam);xlim([-N/2 N/2]);
+subplot(224)
+plot(t,y);xlim([-N/2 N/2]);
+
+%%
+%----------------PSD av utsignaler----------------
+%------------Periodogram----------------------
+w = linspace(0,1,N);
+
+Rypsq = PeriodFourier(zsq);
+Ryphw = PeriodFourier(zhw);
+Rypam = PeriodFourier(zam);
+
+figure(3);
+subplot(222);
+plot(w,Rypsq); title('Raw Periodogram of sq');
+xlabel('[\theta]')
+subplot(223);
+plot(w,Ryphw);title('Raw Periodogram of hw');
+xlabel('[\theta]')
+subplot(224);
+plot(w,Rypam); title('Raw Periodogram of am');
+xlabel('[\theta]')
+%%
+%----------------PerAv-------------------------------
+k = linspace(0,1,2^7);
+
+RypAvsq = PerAv(zsq,2^7);
+RypAvhw = PerAv(zhw,2^7);
+RypAvam = PerAv(zam,2^7);
+
+figure(4);
+subplot(222);
+plot(k,RypAvsq); title('Averaged Periodogram of sq');
+xlabel('[\theta]')
+subplot(223);
+plot(k,RypAvhw);title('Averaged Periodogram of hw');
+xlabel('[\theta]')
+subplot(224);
+plot(k,RypAvam); title('Averaged Periodogram of am');
+xlabel('[\theta]')
+
+%%
+%-------------Smoothed periodogram-------------------
+w2 = window(@blackmanharris,65);
+w3 = window(@triang,65);
+
+rysq = EstimateACF(zsq,'Blett');
+ryhw = EstimateACF(zhw,'Blett');
+ryam = EstimateACF(zam,'Blett');
+
+RypSsq = windowing2(rysq,65, w2);
+RypSsq = windowing2(rysq,65,w3);
+
+RypShw = windowing2(ryhw,65, w2);
+RypShw = windowing2(ryhw,65,w3);
+
+RypSam = windowing2(ryam,65, w2);
+RypSam = windowing2(ryam,65,w3);
+
+figure(3);
+subplot(222);
+plot(w,RypSsq); title('Smoothed Periodogram of sq');
+xlabel('[\theta]')
+subplot(223);
+plot(w,RypShw);title('Smoothed Periodogram of hw');
+xlabel('[\theta]')
+subplot(224);
+plot(w,RypSam); title('Smoothed Periodogram of am');
+xlabel('[\theta]')
