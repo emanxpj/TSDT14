ce74408184fa74e3d8022feb9a5aa0807d23d242
Jens Edhammer <tobyndax@hotmail.com>
2014-10-14 14:18:42 +0200

    Printing of figures and result and theory in report

--- Study3/main.m
+++ Study3/main.m
@@ -35,18 +35,6 @@ RdecT = 1/8*(rectpuls((w-1/2)/(2*wc))) + 1/4*(rectpuls(w/(2*wc))+rectpuls((w-1)/
 Ry = PeriodFourier(y);
 Ralt = PeriodFourier(yalt);
 Rdec  = PeriodFourier(ydec);
-%w2 = window(@blackmanharris,65);
-
-%ry = EstimateACF(y,'Blett');
-%ralt = EstimateACF(yalt,'Blett');
-%rdec = EstimateACF(ydec,'Blett');
-
-%Ry = windowing2(ry,65, w2);
-
-%Ralt = windowing2(ralt,65, w2);
-
-%Rdec = windowing2(rdec,65, w2);
-
 
 figure;
 subplot(311);
@@ -62,4 +50,35 @@ plot(w,Rdec); title('Raw Periodogram of Dec');
 hold on; plot(w,RdecT,'r'); hold off;
 xlabel('[\theta]')
 
-%se sida 176 för teori
+print -depsc ../Report/PSD3.eps
+
+w2 = window(@blackmanharris,65);
+
+ry = EstimateACF(y,'Blett');
+ralt = EstimateACF(yalt,'Blett');
+rdec = EstimateACF(ydec,'Blett');
+
+Ry = windowing2(ry,65, w2);
+
+Ralt = windowing2(ralt,65, w2);
+
+Rdec = windowing2(rdec,65, w2);
+
+
+
+
+figure;
+subplot(311);
+plot(w,Ry); title('Raw Periodogram of y');
+hold on; plot(w,RyT,'r'); hold off; ylim([0 1.2]);
+xlabel('[\theta]')
+subplot(312);
+plot(w,Ralt);title('Raw Periodogram of Alt');
+hold on; plot(w,RaltT,'r'); hold off;
+xlabel('[\theta]')
+subplot(313);
+plot(w,Rdec); title('Raw Periodogram of Dec');
+hold on; plot(w,RdecT,'r'); hold off;
+xlabel('[\theta]')
+
+print -depsc ../Report/PSD3smooth.eps
